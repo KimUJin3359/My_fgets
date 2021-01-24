08ccc3cb2f234ca5cd76819b5ce15e8f1b4bd354
KimUJin3359 <wj3359@naver.com>
2021-01-24 13:03:55 +0900

    first_commit

---
+++ get_next_line_bonus.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: woojikim <woojikim@student.42seoul.>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/30 10:15:27 by woojikim          #+#    #+#             */
+/*   Updated: 2021/01/03 20:37:44 by woojikim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+int		ft_check_line(char *file)
+{
+	int	i;
+
+	i = 0;
+	while (file && file[i])
+	{
+		if (file[i] == '\n')
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+int		ft_get_line(char **file, char **line, int i)
+{
+	char	*temp;
+
+	*line = ft_strldup(*file, i + 1);
+	temp = ft_strdup(*file + i + 1);
+	free(*file);
+	*file = temp;
+	return (1);
+}
+
+int		get_next_line(int fd, char **line)
+{
+	static char	*file[OPEN_MAX];
+	char		buf[BUFFER_SIZE + 1];
+	int			ret;
+	int			i;
+
+	if (fd < 0 || !line || BUFFER_SIZE <= 0)
+		return (-1);
+	if ((i = ft_check_line(file[fd])) != -1)
+		return (ft_get_line(&file[fd], line, i));
+	while ((ret = read(fd, buf, BUFFER_SIZE)) > 0)
+	{
+		buf[ret] = 0;
+		file[fd] = ft_strjoin(file[fd], buf);
+		if ((i = ft_check_line(file[fd])) != -1)
+			return (ft_get_line(&file[fd], line, i));
+	}
+	if (ret < 0)
+		return (-1);
+	if (file[fd])
+		*line = file[fd];
+	else
+		*line = ft_strdup("");
+	file[fd] = NULL;
+	return (0);
+}
---
+++ get_next_line_bonus.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: woojikim <woojikim@student.42seoul.>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/30 10:13:16 by woojikim          #+#    #+#             */
+/*   Updated: 2021/01/03 20:40:10 by woojikim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include "unistd.h"
+# include "stdlib.h"
+# include "fcntl.h"
+# include "stdio.h"
+# include "limits.h"
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 128
+# endif
+
+int			get_next_line(int fd, char **line);
+char		*ft_strldup(const char *s1, size_t size);
+char		*ft_strdup(const char *s1);
+char		*ft_strjoin(char *s1, char *s2);
+size_t		ft_strlen(const char *s);
+
+#endif
